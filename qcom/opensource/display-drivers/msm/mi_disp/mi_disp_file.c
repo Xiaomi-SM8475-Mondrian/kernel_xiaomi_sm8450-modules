cupid-development/android_kernel_xiaomi_sm8450-modules

display: mi_disp: Allow partial reads
Change-Id: I7f9720153a7992b4fe0526e2114b780674465ec6

qcom/opensource/display-drivers/msm/mi_disp/mi_disp_file.c
@@ -136,15 +136,16 @@ ssize_t mi_disp_read(struct file *filp, char __user *buffer,
 				return ret;
 		} else {
 			unsigned length = e->event.base.length;
+			unsigned copy_length = length - e->offset;
+			bool partial_read = false;
 
-			if (length > count - ret) {
-put_back_event:
-				spin_lock_irq(&df->client_spinlock);
-				client->event_space -= length;
-				list_add(&e->link, &client->event_list);
-				spin_unlock_irq(&df->client_spinlock);
-				wake_up_interruptible(&client->event_wait);
-				break;
+			DISP_DEBUG("length is: %d, offset is: %d, count is %d",
+					length, e->offset, count);
+
+			if (copy_length > count - ret) {
+				copy_length = count - ret;
+
+				partial_read = true;
 			}
 
 			DISP_DEBUG("%s display event type: %s\n",
@@ -152,15 +153,30 @@ put_back_event:
 				get_disp_event_type_name(e->event.base.type));
 			DISP_DEBUG("%s display event length: %d\n",
 				get_disp_id_name(e->event.base.disp_id), length);
+			DISP_INFO("%s display event copy length: %d\n",
+				get_disp_id_name(e->event.base.disp_id), copy_length);
 
-			if (copy_to_user(buffer + ret, &e->event, length)) {
+			if (copy_to_user(buffer + ret, ((__u8*)&e->event) +
+					e->offset, copy_length)) {
 				if (ret == 0)
 					ret = -EFAULT;
 				goto put_back_event;
 			}
 
-			ret += length;
-			kfree(e);
+			ret += copy_length;
+			if (partial_read) {
+				e->offset += count;
+put_back_event:
+				DISP_INFO("putting event back!");
+				spin_lock_irq(&df->client_spinlock);
+				client->event_space -= length;
+				list_add(&e->link, &client->event_list);
+				spin_unlock_irq(&df->client_spinlock);
+				wake_up_interruptible(&client->event_wait);
+				break;
+			} else {
+				kfree(e);
+			}
 		}
 	}
 	mutex_unlock(&client->event_lock);

Xiaomi-SM8475-Mondrian/kernel_xiaomi_sm8450-modules

qcom/opensource/display-drivers/msm/mi_disp/mi_disp_file.c
@@ -136,15 +136,16 @@ ssize_t mi_disp_read(struct file *filp, char __user *buffer,
 				return ret;
 		} else {
 			unsigned length = e->event.base.length;
+			unsigned copy_length = length - e->offset;
+			bool partial_read = false;
 
-			if (length > count - ret) {
-put_back_event:
-				spin_lock_irq(&df->client_spinlock);
-				client->event_space -= length;
-				list_add(&e->link, &client->event_list);
-				spin_unlock_irq(&df->client_spinlock);
-				wake_up_interruptible(&client->event_wait);
-				break;
+			DISP_DEBUG("length is: %d, offset is: %d, count is %d",
+					length, e->offset, count);
+
+			if (copy_length > count - ret) {
+				copy_length = count - ret;
+
+				partial_read = true;
 			}
 
 			DISP_DEBUG("%s display event type: %s\n",
@@ -152,15 +153,30 @@ put_back_event:
 				get_disp_event_type_name(e->event.base.type));
 			DISP_DEBUG("%s display event length: %d\n",
 				get_disp_id_name(e->event.base.disp_id), length);
+			DISP_INFO("%s display event copy length: %d\n",
+				get_disp_id_name(e->event.base.disp_id), copy_length);
 
-			if (copy_to_user(buffer + ret, &e->event, length)) {
+			if (copy_to_user(buffer + ret, ((__u8*)&e->event) +
+					e->offset, copy_length)) {
 				if (ret == 0)
 					ret = -EFAULT;
 				goto put_back_event;
 			}
 
-			ret += length;
-			kfree(e);
+			ret += copy_length;
+			if (partial_read) {
+				e->offset += copy_length;
+put_back_event:
+				DISP_INFO("putting event back!");
+				spin_lock_irq(&df->client_spinlock);
+				client->event_space -= length;
+				list_add(&e->link, &client->event_list);
+				spin_unlock_irq(&df->client_spinlock);
+				wake_up_interruptible(&client->event_wait);
+				break;
+			} else {
+				kfree(e);
+			}
 		}
 	}
 	mutex_unlock(&client->event_lock);